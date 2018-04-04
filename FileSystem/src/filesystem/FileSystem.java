412b1259010c8e17416bc5d64f07648dff68a25e
Rhalp Darren Cabrera <rhalpdarrencabrera@gmail.com>
2018-04-04 14:54:09 +0800

    Update FileSystem.java

--- FileSystem/src/filesystem/FileSystem.java
+++ FileSystem/src/filesystem/FileSystem.java
@@ -8,6 +8,7 @@ package filesystem;
 import java.util.Scanner;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.io.File;
 
 /**
  *
@@ -27,47 +28,75 @@ public class FileSystem {
         int option;
 
  	do {
-            System.out.println("Choose transaction:");
-            System.out.println("Press 'w' to withdraw");
-            System.out.println("Press 'b' to check balance");
-            System.out.println("Press 'd' to deposit");
-            System.out.println("Press 'x' to exit");
+            System.out.println("Please choose an option:");
+            System.out.println("1. Create a directory\n" +
+"2. Create multiple directory\n" +
+"3. Create file\n" +
+"4. Rename a directory/file\n" +
+"5. Copy directory/file\n" +
+"6. Move directory/file\n" +
+"7. Hide directory/file\n" +
+"8. Unhide directory/file\n" +
+"9. Write to a text file\n" +
+"10. Read text file\n"+
+"0. Exit\n");
             option = sc.nextInt();
             switch (option) {
                 case 0:
+                        System.out.println("System Exit");
 			System.exit(0);
                     break;
                 case 1:
-                    String user_path = sc.next();
-                    Path path = Paths.get("C:\\"+user_path);
+                    System.out.println("Path Source:");
+                    String user_path1 = sc.next();
+                    Path path = Paths.get("C:\\"+user_path1);
                     f_func.mkDirectory(path.toString());
                     break;
                 case 2:
-                    f_func.mkDirectory("C:\\sample");
+                    System.out.println("Path Source:");
+                    //\\sample2\\sub1\\sub2
+                    String user_path2 = sc.next();
+                    f_func.mkMulipleDirectory("C:\\"+user_path2);
                     break;
                 case 3:
-                    f_func.mkDirectory("C:\\sample");
+                    System.out.println("Path Source:");
+                    String user_path3 = sc.next();
+                    f_func.mkFile("C:\\"+user_path3, "sample", ".txt");
                     break;
                 case 4:
-                    f_func.mkDirectory("C:\\sample");
+                    File src = new File("C:\\sample");
+                    System.out.println("New File Name:");
+                    String user_fname = sc.next();
+                    f_func.renameDir(src, user_fname);
                     break;
                 case 5:
-                    f_func.mkDirectory("C:\\sample");
+                    File src1 = new File("C:\\sample");
+                    File dest = new File("C:\\sample2\\sub1");
+                    f_func.copyFile(src1, dest);
                     break;
                 case 6:
-                    f_func.mkDirectory("C:\\sample");
+                    File src2 = new File("C:\\sample");
+                    File dest1 = new File("C:\\sample2\\sub1");
+                    f_func.moveFile(src2, dest1);
                     break;
                 case 7:
-                    f_func.mkDirectory("C:\\sample");
+//                    f_func.mkDirectory("C:\\sample");
                     break;
                 case 8:
-                    f_func.mkDirectory("C:\\sample");
+                    
+                    System.out.println("Path Source:");
+                    String user_path8 = sc.next();
+                    String path8 = "C:\\"+user_path8;
+                    f_func.hideFile(path8);
                     break;
                 case 9:
-                    f_func.mkDirectory("C:\\sample");
+                    System.out.println("Path Source:");
+                    String user_path9 = sc.next();
+                    String path9 = "C:\\"+user_path9;
+                    f_func.unHideFile(path9);
                     break;
                 case 10:
-                    f_func.mkDirectory("C:\\sample");
+                    f_func.viewfile();
                     break;
                 default:
                     System.out.println("Invalid input!");
